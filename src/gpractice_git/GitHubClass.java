e41f20e33a8e9c19bdb59ceb5ee977aad2550e6e
unknown <yasirking588@gmail.com>
2022-06-08 20:07:55 -0400

    Hey I added The GitHub Code

---
+++ src/gpractice_git/GitHubClass.java
@@ -0,0 +1,17 @@
+package gpractice_git;
+
+public class GitHubClass {
+
+	public static void main(String[] args) {
+		
+		
+		int a= 12;
+		int b=25;
+		
+		
+		int c= a+b;
+		System.out.println(c);
+
+	}
+
+}
